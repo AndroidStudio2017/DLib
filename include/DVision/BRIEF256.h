7034a7fa3c0ee607d7c652e0281cb9c7647c580e
AndroidStudio2017 <17319255967@163.com>
2020-10-17 10:15:38 +0800

    Update BRIEF256.h

--- include/DVision/BRIEF256.h
+++ include/DVision/BRIEF256.h
@@ -29,6 +29,9 @@
 #ifndef __D_BRIEF_256__
 #define __D_BRIEF_256__
 
+// Add by Johnson
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <vector>
