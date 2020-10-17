327e2e8597b74f01dd59a9a0f533850a6be09b18
AndroidStudio2017 <17319255967@163.com>
2020-10-17 10:45:47 +0800

    Update BIERF256.h --

--- include/DVision/BRIEF256.h
+++ include/DVision/BRIEF256.h
@@ -31,7 +31,7 @@
 
 // Add by Johnson
 #include <opencv2/imgproc/imgproc.hpp>
-#include <DUtils.h>
+#include <DUtils/DUtils.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
